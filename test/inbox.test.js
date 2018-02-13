4e998e483f41c94307ea480a1013052b08926563
Khai Bui <khaibui@Khais-MacBook-Pro.local>
2018-02-12 22:56:03 -0800

    Clean up some linter stuff

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const { interface: compiledInterface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
@@ -12,7 +13,7 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // Use 1 account to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(JSON.parse(compiledInterface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 });
